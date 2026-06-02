111472870d23d3fdc92afbf858aa1db21a8d916a
Laurent Toutain <Laurent@Touta.in>
2026-06-02 22:25:12 +0200

    chore: bump versionCode to 3 and versionName to 1.2

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "fr.schcchair.coreconf_m2m"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
